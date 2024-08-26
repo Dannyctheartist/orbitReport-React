94c29f8a489800f6c28f6046da9ed6b7f7cb71a0
unknown <danc46436@gmail.com>
2024-08-26 01:36:05 -0500

    Completed Assignment 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import React, { useState } from "react";
+import satData from "./components/satData.jsx"; 
 
 
 function App() {
+ const [sat, setSat] = useState(satData);
+ const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+ const filterByType = (currentType) => {
+  console.log("Filtering for type:", currentType);
+  const filteredSats = satData.filter((data) => data.orbitType === currentType);
+  console.log("Filtered satallites:", filteredSats);
+  setSat(filteredSats);
+ };
+ 
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner/>
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats} />
+      <Table sat={sat}/>
     </div>
   );
 }
